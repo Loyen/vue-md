ff5a0aa8770bd10609b3523216aa9a2f96a4c326
Patrik Freij <patrik.t.freij@gmail.com>
2017-04-01 14:54:37 +0200

    Add icon component
    Size is default "small" to fit inline with text correctly.

---
+++ src/components/md-icon/md-icon.vue
@@ -0,0 +1,47 @@
+<template>
+	<div class="icon" :class="'icon--'+size">{{ name }}</div>
+</template>
+
+<script>
+export default {
+	props: {
+		name: { required: true },
+		size: { default: 'small' }
+	}
+}
+</script>
+
+
+<style>
+@import url('https://fonts.googleapis.com/icon?family=Material+Icons');
+
+.icon {
+	font-family: 'Material Icons';
+	font-weight: normal;
+	font-style: normal;
+	display: inline-block;
+	line-height: 1;
+	text-transform: none;
+	letter-spacing: normal;
+	word-wrap: normal;
+	white-space: nowrap;
+	direction: ltr;
+	vertical-align: sub;
+}
+
+.icon--small { /* 18 */
+	font-size: 1.2em;
+}
+
+.icon--medium { /* 24 */
+	font-size: 1.5em;
+}
+
+.icon--large { /* 36 */
+	font-size: 2.3em;
+}
+
+.icon--larger { /* 48 */
+	font-size: 3em;
+}
+</style>
