d000a498aa40fe99380688c81502a674cba4408a
Patrik Freij <patrik.t.freij@gmail.com>
2017-04-22 18:25:31 +0200

    Use input field when not multirow and function cleaning

--- src/components/md-textfield/md-textfield.vue
+++ src/components/md-textfield/md-textfield.vue
@@ -3,6 +3,8 @@
 		<div class="textfield" :class="{ isTouched: focus || hasText() }">
 			<div class="textfield-label" :class="accentForeground">{{ label }}</div>
 			<div v-if="focus && !hasText()" class="textfield-placeholder">{{ placeholder }}</div>
+			<textarea v-if="multiline" :rows="rows" class="textfield-input" v-model="text" @focus="focus = true" @blur="focus = false"></textarea>
+			<input type="text" v-if="!multiline" class="textfield-input" v-model="text" @focus="focus = true" @blur="focus = false" />
 			<div class="textfield-line" :class="accentBackground"></div>
 			<div v-if="description" class="textfield-helper">{{ description }}</div>
 		</div>
@@ -22,6 +24,7 @@ export default {
 	data() {
 		return {
 			text: '',
+			rows: 1,
 			focus: false,
 			accentForeground: '',
 			accentBackground: ''
@@ -33,6 +36,12 @@ export default {
 	watch: {
 		focus: function() {
 			this.setAccent();
+		},
+		text: function() {
+			if (!this.multiline) return;
+
+			let text_split = this.text.split(/\r\n|\r|\n/);
+			this.rows = text_split.length;
 		}
 	},
 	methods: {
@@ -47,16 +56,6 @@ export default {
 		hasText() {
 			return this.text.length > 0;
 		},
-		rowCount() {
-			if (!this.multiline) return 1;
-
-			let text_split = this.text.split(/\r\n|\r|\n/);
-			return text_split.length;
-		},
-		cleanText() {
-			if (!this.multiline)
-				this.text = this.text.replace(/\r\n|\r|\n/gm, '');
-		}
 	}
 }
 </script>
